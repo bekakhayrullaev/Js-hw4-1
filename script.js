ded8bfc3a52d98f2da7bd04dcd10ec1bbdf30c69
[200~bekakhayrullaev <bekahajrullaev27@gmail.com>
2022-03-12 20:47:06 +0500

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
+let arr = [1, 2, false, 'hello', 24, 'world', 20 ,undefined, null, 'error', 22]
 
-let arr = [1,2,false,'hello',24,'world',undefined,null,'error',22];
-
-
-let filtered = arr.filter(item => typeof item === 'number')
-console.log(filtered);
-
-let str = arr.filter(item => typeof item === 'string')
-console.log(str);
-
-let bool = arr.filter(item => typeof item === 'boolean')
-console.log(bool);
-
-let obj = arr.filter(item => typeof item === 'object')
-
-console.log(obj);
-
-let find = arr.filter(item => typeof item === 'underfined')
-
-console.log(find );
+if(arr.length >= 5){
+    console.log('Good')
+} else{
+    console.log('my bad')
+}
 
